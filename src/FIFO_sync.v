716875b641a50cd4232b611f5043484b19351e15
Yumin Chiang <s9831498@gmail.com>
2025-11-22 22:19:10 +0800

    feat(src): re-add FIFO_sync.v

---
+++ src/FIFO_sync.v
@@ -0,0 +1,16 @@
+module FIFO_sync(
+    input             clk     ,
+    input             rst     ,
+    input             wr_en   ,
+    input             rd_en   ,
+    input       [7:0] data_in ,
+    output            full    ,
+    output            empty   ,
+    output reg  [7:0] data_out
+);
+
+/*
+	Write Your Design Here ~
+*/
+
+endmodule
